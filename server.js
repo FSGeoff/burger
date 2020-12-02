4e52b958f97044bf254acf15cc0345caadfff403
Geoff Nix <FullStackGeoff@gmail.com>
2020-12-02 13:40:55 -0500

    added partials folder

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-const routes = require("./controllers/burgers_controller.js");
+const routes = require("./controllers/burgers_controllers");
 app.use(routes);
 app.listen(PORT, function() {
     // Log (server-side) when our server has started
