131afde74b8202e837dc87babe072b3dfa65bbbd
Geoff Nix <FullStackGeoff@gmail.com>
2020-12-02 22:59:46 -0500

    added heroku jawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,17 @@
 var mysql = require("mysql");
-var connection = mysql.createConnection({
-	host: "localhost",
-	port: 3306,
-	user: "root",
-	password: "7Streams!",
-	database: "burgers_db",
-});
+var connection; 
+
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "7Streams!",
+    database: "burger_db"
+  })
+};
 // Make connection.
 connection.connect(function (err) {
 	if (err) {
@@ -16,3 +22,4 @@ connection.connect(function (err) {
 });
 // Export connection for our ORM to use.
 module.exports = connection;
+
