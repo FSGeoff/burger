383b68abd01bdf54af856e4ef3dfa3da9334c09e
Geoff Nix <FullStackGeoff@gmail.com>
2020-12-02 22:45:54 -0500

    heroku

--- models/burger.js
+++ models/burger.js
@@ -2,19 +2,19 @@ const orm = require("../config/orm");
 
 const burger = {
 	selectAll: function (cb) {
-		orm.selectAll("burgers", function (res) {
-			cb(res);
-		});
+		// orm.selectAll("burgers", function (res) {
+		// 	cb(res);
+		// });
 	},
 	insertOne: function (cols, vals, cb) {
-		orm.insertOne("burgers", cols, vals, function (res) {
-			cb(res);
-		});
+		// orm.insertOne("burgers", cols, vals, function (res) {
+		// 	cb(res);
+		// });
 	},
 	updateOne: function (objColVals, condition, cb) {
-		orm.updateOne("burgers", objColVals, condition, function (res) {
-			cb(res);
-		});
+		// orm.updateOne("burgers", objColVals, condition, function (res) {
+		// 	cb(res);
+		// });
 	},
 };
 module.exports = burger;
