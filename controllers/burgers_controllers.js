a166f4c6f13fddf44c478a1e5abd7a12dfc467a0
Geoff Nix <FullStackGeoff@gmail.com>
2020-12-03 22:14:33 -0500

    finally got html to show

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -3,37 +3,44 @@ const burger = require("../models/burger");
 const router = express.Router();
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
-  burger.selectAll(function(data) {
-    var hbsObject = {
-      burger: data
-    };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
-  });
+router.get("/", function (req, res) {
+	burger.all(function (data) {
+		var hbsObject = {
+			burger: data,
+		};
+		console.log(hbsObject);
+		res.render("index", hbsObject);
+	});
 });
-router.post("/api/burgers", function(req, res) {
-  burger.insertOne([
-    "name", "devoured"
-  ], [
-    req.body.name, req.body.devoured
-  ], function(result) {
-    // Send back the ID of the new quote
-    res.json({ id: result.insertId });
-  });
+router.post("/api/burgers", function (req, res) {
+  console.log(req.body);
+	burger.create(
+		["name", "devoured"],
+		[req.body.name, req.body.devoured],
+		function (result) {
+			// Send back the ID of the new quote
+      res.json({ id: result.insertId });
+      console.log(res.json);
+		}
+	);
 });
-router.put("/api/burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
+router.put("/api/burgers/:id", function (req, res) {
+	var condition = "id = " + req.params.id;
   console.log("condition", condition);
-  burger.updateOne({
-    devoured: req.body.devoured
-  }, condition, function(result) {
-    if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
+  console.log(req.body);
+	burger.update(
+		{
+			devoured: req.body.devoured,
+		},
+		condition,
+		function (result) {
+			if (result.changedRows == 0) {
+				// If no rows were changed, then the ID must not exist, so 404
+				return res.status(404).end();
+			} else {
+				res.status(200).end();
+			}
+		}
+	);
 });
 module.exports = router;
