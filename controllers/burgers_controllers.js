12cd8ad455f5fe0bfec6e6ac5bdaee01172b783e
Geoff Nix <FullStackGeoff@gmail.com>
2020-12-03 21:18:11 -0500

    modified a few files

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const burger = require("../models/burger");
 const router = express.Router();
+
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
   burger.selectAll(function(data) {
